634263b4ca1f4eb0e7509b7c014003bd9e4fbbc7
madv4 <sleeptherank@gmail.com>
2026-03-30 16:23:40 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/4madvape/madvape/'..readfile('vaperewrite/profiles/commit.txt')..'/'..select(1, path:gsub('vaperewrite/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/4madv4/madv4/'..readfile('vaperewrite/profiles/commit.txt')..'/'..select(1, path:gsub('vaperewrite/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -47,7 +47,7 @@ local function downloadPremadeProfiles(commit)
     end
 
     local success, response = pcall(function()
-        return game:HttpGet('https://api.github.com/repos/4madvape/madvape/contents/profiles/premade?ref=' .. commit)
+        return game:HttpGet('https://api.github.com/repos/4madv4/madv4/contents/profiles/premade?ref=' .. commit)
     end)
 
     if success and response then
@@ -76,7 +76,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/4madvape/madvape')
+		return game:HttpGet('https://github.com/4madv4/madv4')
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
